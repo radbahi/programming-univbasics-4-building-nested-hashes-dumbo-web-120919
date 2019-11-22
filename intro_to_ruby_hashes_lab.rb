fb2994f5a86051fabd178dd74a16a895c5331ca7
Radouane Bahi <radbahi@gmail.com>
2019-11-22 23:10:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,17 +1,40 @@
 
 def base_hash
+<<<<<<< HEAD
+=======
+  # Remember implicit return! We're returning this new Hash _without_ the
+  # keyword "return." Nice and neat.
+>>>>>>> 2bfe1c61c4ab8635f8d6706fb87b42e35670a8a7
   hash = { :railroads => {} }
 end
 
 def monopoly_with_second_tier
+<<<<<<< HEAD
+=======
+  # When you start writing the implementation for this method, copy the Hash
+  # you built in the previous method. Run the tests. They will fail, but
+  # they'll guide you in how to modify what you just did to have more
+  # complexity.
+>>>>>>> 2bfe1c61c4ab8635f8d6706fb87b42e35670a8a7
   hash = { :railroads => {pieces: 4 } }
 end
 
 def monopoly_with_third_tier
+<<<<<<< HEAD
   hash = { :railroads => 
   {:names => {:reading_railroad => {} , :pennsylvania_railroad => {} , :b_and_o_railroad => {} , :shortline_railroad => {} }, 
 
   :rent_in_dollars => {one_piece_owned: 25 , two_pieces_owned: 50 , three_pieces_owned: 100 , four_pieces_owned: 200} 
+=======
+  # When you start writing the implementation for this method, copy the Hash
+  # you built in the previous method. Run the tests. They will fail, but
+  # they'll guide you in how to modify what you just did to have more
+  # complexity.
+  hash = { :railroads => 
+  {:names => {firstkey: 1 , secondkey: 2 , thirdkey: 3 , fourthkey: 4}, 
+
+  :rent_in_dollars => {firstkey: 25 , secondkey: 2 , thirdkey: 3 , fourthkey: 4} 
+>>>>>>> 2bfe1c61c4ab8635f8d6706fb87b42e35670a8a7
   }
   }
 end
